a1d2442e4b2d1a336bedfdb9ae209773ddd3be7e
Monde MBATHA <mmbatha@e5r4p4.wethinkcode.co.za>
2018-06-18 12:12:22 +0200

    Get_Next_Line tests

---
+++ main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmbatha <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/06 13:34:20 by mmbatha           #+#    #+#             */
+/*   Updated: 2018/06/06 17:36:36 by mmbatha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "libft/includes/get_next_line.h"
+
+int				main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+	{
+		fd = 0;
+		return (0);
+	}
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+	}
+	if (argc == 2)
+		close(fd);
+}
